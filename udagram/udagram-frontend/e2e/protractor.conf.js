e71205009a9ae4c9f46a01b16110a8bc259db481
Mazen Sherif <m.sherifabdelaal@gmail.com>
2022-12-06 04:54:27 +0200

    update

--- udagram/udagram-frontend/e2e/protractor.conf.js
+++ udagram/udagram-frontend/e2e/protractor.conf.js
@@ -5,25 +5,24 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: ["./src/**/*.e2e-spec.ts"],
+  specs: [
+    './src/**/*.e2e-spec.ts'
+  ],
   capabilities: {
-    browserName: "chrome",
-    binary: '/usr/local/bin',
+    'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: "http://localhost:4200/",
-  framework: "jasmine",
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function () {},
+    print: function() {}
   },
   onPrepare() {
-    require("ts-node").register({
-      project: require("path").join(__dirname, "./tsconfig.e2e.json"),
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine
-      .getEnv()
-      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  },
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  }
 };
