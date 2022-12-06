ff7eebd1509e4faffba4d9c1f7ef7d4d8a34b73c
omaryasserreda9 <m.sherifabdelaal@gmail.com>
2022-12-06 15:38:30 +0200

    update

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-env.eba-jvdeqfj9.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-env-1.eba-zmmzmqiq.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
